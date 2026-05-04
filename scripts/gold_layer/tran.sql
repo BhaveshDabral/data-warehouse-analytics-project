85d89a5e5e909c3852303d2c5e205196b58b432a
Bhavesh Dabral <Bhaveshdabral.work@gmail.com>
2026-05-04 14:05:10 +0530

    Delete scripts directory

--- scripts/gold_layer/tran.sql
+++
@@ -1,7 +0,0 @@
--- forgein key interigtiy (dimension)
-select * from gold.fact_sales f 
-left join gold.dim_customers c 
-on c.customer_key = f.customer_key
-left join gold.dim_products p 
-on p.product_key = f.product_key
-where p.product_key is null 
